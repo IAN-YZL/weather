7da1107c0bed13b02c1c5078c39c8d51dca39b96
Ian Yin <yin.ian093@gmail.com>
2020-08-25 14:59:54 +1000

    add bg img

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Banner  from '../Banner';
 import Future from '../Future';
 import './App.css';
+import style from './App.module.scss';
 
 
 function App() {
   return (
-    <div className="App">
-        <Banner temp={12} >Cloudy</Banner>
-        <Future />
-
+    <div className={style.container}>
+      <div className={style.App}>
+          <Banner temp={12} >Cloudy</Banner>
+          <Future />
+      </div>
     </div>
   );
 }
