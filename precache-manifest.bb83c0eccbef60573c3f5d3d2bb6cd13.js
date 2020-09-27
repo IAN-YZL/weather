b19a7a46543891bcafcdf404d485134b0ecfcbf1
IANY <yin.ian093@gmail.com>
2020-09-27 21:39:36 +1000

    Updates

---
+++ precache-manifest.bb83c0eccbef60573c3f5d3d2bb6cd13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4a3bb5a097c7761ec9b66b38c0d8c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3e54454fa1448c4129d",
+    "url": "./static/css/main.bb846fae.chunk.css"
+  },
+  {
+    "revision": "28c70966114755753340",
+    "url": "./static/js/2.fa891303.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fa891303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e54454fa1448c4129d",
+    "url": "./static/js/main.26ec9ee1.chunk.js"
+  },
+  {
+    "revision": "7185123dbfbf19a73c74",
+    "url": "./static/js/runtime-main.3def4396.js"
+  },
+  {
+    "revision": "50919ea325d50cf184810d09b5b922c4",
+    "url": "./static/media/Paris.50919ea3.jpg"
+  },
+  {
+    "revision": "d5b633045d67b8c9329dc80eb91e5d6e",
+    "url": "./static/media/bg.d5b63304.jpg"
+  }
+]);
