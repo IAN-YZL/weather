fea5ba88aea3b2990acc0f11e103f1253fa01f23
IAN-YZL <yin.ian093@gmail.com>
2020-12-06 11:23:20 +1000

    💥 Change the banner image

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import style from './Banner.module.scss';
 import WeatherInfo from './WeatherInfo';
 import City from './City';
+import logo from './assets/banner.png';
 
 const Banner = () => (
-    <div className={style.banner}>
+    <div className={style.banner} style={{ backgroundImage: `url(${logo})` }}>
         <WeatherInfo />
         <City />
     </div>
